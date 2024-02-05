e6f5de1ed965b3485611d33dda49cbf27851df7f
dfahlander <david.fahlander@gmail.com>
2024-02-05 17:44:41 +0100

    Stripped out lot of dexie resources

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 #bundle exec jekyll serve --incremental
-bundle exec jekyll serve
+bundle exec jekyll serve -P 4100
 #echo $! > jekyll.pid
 #echo "Surf to http://localhost:4000"
 #echo "To stop jekyll and http-server, run ./stop.sh"
